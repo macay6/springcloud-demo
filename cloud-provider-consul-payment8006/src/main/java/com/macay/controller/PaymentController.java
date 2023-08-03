0032545902c9904aa1b31928920d915c286547dc
macay <1207260521@qq.com>
2023-08-03 16:17:03 +0800

    Springcloud之consul替换Eureka作为服务注册中心

---
+++ cloud-provider-consul-payment8006/src/main/java/com/macay/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.macay.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+import java.util.UUID;
+
+/**
+ * @ClassName: PaymentController
+ * @Description:
+ * @Author: Macay
+ * @Date: 2023/3/28 5:59 下午
+ */
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")        //获取端口号
+    private String serverPort;
+
+    @RequestMapping("/payment/consul")
+    public String paymentzk(){
+        return "springcloud with consul：" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
