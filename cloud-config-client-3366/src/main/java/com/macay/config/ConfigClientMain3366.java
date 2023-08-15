ffc5d6c7f1cc9214ffb4abba8990bbbcd01b78e9
macay <1207260521@qq.com>
2023-08-15 11:01:38 +0800

    SpringCloud之消息总线Bus

---
+++ cloud-config-client-3366/src/main/java/com/macay/config/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.macay.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName: ConfigClientMain3355
+ * @Description:
+ * @Author: Macay
+ * @Date: 2023/8/14 5:50 下午
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
